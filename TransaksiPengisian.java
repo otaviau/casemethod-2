d4190a5df2c24c3eaa9e8788c76c0ceea157dabc
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2025-06-04 11:20:18 +0700

    TransaksiPengisian

---
+++ TransaksiPengisian.java
@@ -0,0 +1,11 @@
+public class TransaksiPengisian {
+    Kendaraan kendaraan;
+    BBM bbm;
+    double liter, totalBayar;
+    TransaksiPengisian (Kendaraan kendaraan, BBM bbm, double liter) {
+        this.kendaraan = kendaraan;
+        this.bbm = bbm;
+        this.liter = liter;
+        this.totalBayar = hitungTotalBayar();
+    }
+}
