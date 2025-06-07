fe878d54aee1c2540ac6f805760acdafe56ac799
uotavia <uotavia@gmail.com>
2025-06-07 18:23:41 +0700

    update node and bbm

--- BBM.java
+++ BBM.java
@@ -2,14 +2,8 @@ public class BBM {
     String namaBBM;
     double hargaPerLiter;
 
-    public BBM(String namaBBM, double hargaPerLiter) {
+    BBM(String namaBBM, double hargaPerLiter) {
         this.namaBBM = namaBBM;
         this.hargaPerLiter = hargaPerLiter;
-    }
-    public double getHargaPerLiter() {
-        return hargaPerLiter;
-    }
-    public String getNama() {
-        return namaBBM;
     }
 }
---
+++ NodeKendaraan.java
@@ -0,0 +1,9 @@
+public class NodeKendaraan {
+    Kendaraan data;
+    NodeKendaraan next;
+
+    NodeKendaraan(Kendaraan data) {
+        this.data = data;
+        this.next = null;
+    }
+}
