ba066ec844016f43cfc23d1a6922cce58bc98893
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2025-06-07 18:20:50 +0700

    revisi transaksi

--- TransaksiPengisian.java
+++ TransaksiPengisian.java
@@ -1,19 +1,17 @@
-public class TransaksiPengisian {
+class TransaksiPengisian {
     Kendaraan kendaraan;
     BBM bbm;
-    double liter, totalBayar;
-    TransaksiPengisian (Kendaraan kendaraan, BBM bbm, double liter) {
+    double liter;
+    double totalBayar;
+
+    TransaksiPengisian(Kendaraan kendaraan, BBM bbm, double liter) {
         this.kendaraan = kendaraan;
         this.bbm = bbm;
         this.liter = liter;
-        this.totalBayar = hitungTotalBayar();
+        this.totalBayar = liter * bbm.hargaPerLiter;
     }
- 
-    public void tampil() {
-        System.out.println("Kendaraan: " + kendaraan.getNama());
-        System.out.println("BBM: " + bbm.getNama());
-        System.out.println("Liter: " + liter);
-        System.out.println("Total Bayar: Rp " + totalBayar);
-        System.out.println("---------------------------");
+
+    void tampilkanTransaksi() {
+        System.out.println("Kendaraan: " + kendaraan.platNomor + ", BBM: " + bbm.namaBBM + ", Liter: " + liter + ", Total: " + totalBayar);
     }
 }
