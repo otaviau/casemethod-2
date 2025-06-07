a1ceafc3fe03f9f8cb3c933b57504296aa77cd43
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2025-06-07 18:38:37 +0700

    tampilkendaraan dan jumlah

--- AntrianKendaraan.java
+++ AntrianKendaraan.java
@@ -22,4 +22,27 @@ public class AntrianKendaraan {
         if (head == null) tail = null;
         return keluar;
     }
+    public void tampilkanAntrian() {
+        NodeKendaraan current = head;
+        int no = 1;
+        if (current == null) {
+            System.out.println("Antrian kosong.");
+            return;
+        }
+        while (current != null) {
+            System.out.print(no++ + ". ");
+            current.data.tampilkanInformasi();
+            current = current.next;
+        }
+    }
+
+    public int jumlahAntrian() {
+        int count = 0;
+        NodeKendaraan current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
 }
